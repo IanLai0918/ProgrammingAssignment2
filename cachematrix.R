0fc4f72cdf44b07407ff65293d429b8f046cbba3
IanLai0918 <37571848+IanLai0918@users.noreply.github.com>
2018-05-21 22:27:51 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,7 @@ makeCacheMatrix <- function(x = matrix())
     x
   inverse <- NULL       #make sure the inverse is originally empty
   setinverse <- function(x)       
-    {inverse <<- solve(x)}      #let the variable 'inverse' contains the inverse matrix of the input matrix
+    {inverse <- solve(x)}      #let the variable 'inverse' contains the inverse matrix of the input matrix
   getinverse <- function() 
     {inverse}                #get the inverse matrix
     
